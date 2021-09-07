9c80f907670d05b09a4bd05b849f247478e31faf
Alexandr Matte <alexande.m@maergroup.ru>
2021-09-07 16:01:20 +0300

    first steps - require express

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// start
 let express = require('express');
-// empty
+let app = express();
+app.use(express.static('public'));
+
+app.listen(3000, function(){
+    console.log('express start on port 3000');
+});
+
+app.get('/', function(req, res){
+    console.log('load /');
+    res.render('index.html');
+})
+// end
