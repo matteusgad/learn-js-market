5edf94e1b8c34fbc1794c297d6ea4f617744168d
Alexandr Matte <alexande.m@maergroup.ru>
2021-11-30 16:23:24 +0300

    xz

--- app.js
+++ app.js
@@ -2,13 +2,10 @@
 let express = require('express');
 let app = express();
 app.use(express.static('public'));
-
 app.listen(3000, function(){
     console.log('express start on port 3000');
 });
-
 app.get('/', function(req, res){
-    console.log('load /');
     res.render('index.html');
 })
-// end
+// end 
