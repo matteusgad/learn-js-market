a1588c5a02574df440720146f5f4d81fb8267b13
Alexandr Matte <alexande.m@maergroup.ru>
2021-12-29 15:26:01 +0300

    pug layout

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // start
 let express = require('express');
 let app = express();
-app.use(express.static('public'));
+app.use(express.static('public')); // name folder static
+app.set('view engine', 'pug');
+
+
 app.listen(3000, function(){
     console.log('express start on port 3000');
 });
+
+
+
+
 app.get('/', function(req, res){
-    res.render('index.html');
+    res.render('main', {
+        foo: 1,
+        bar: 2
+    });
+    
+})
+
+app.get('/cat', function(req, res){
+    res.end('cat')
 })
 // end 
