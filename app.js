f8e0a67e1e3eb82f25c9d88f82370e011f54eda9
Alexandr Matte <alexande.m@maergroup.ru>
2022-01-10 16:54:27 +0300

    category, product

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ let app = express();
 app.use(express.static('public')); // name folder static
 app.set('view engine', 'pug');
 
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'market',
+    password: 'market',
+    database: 'market'
+    
+});
+
 
 app.listen(3000, function(){
     console.log('express start on port 3000');
@@ -13,14 +23,64 @@ app.listen(3000, function(){
 
 
 app.get('/', function(req, res){
-    res.render('main', {
-        foo: 1,
-        bar: 2
-    });
+    connection.query(
+        'SELECT * FROM products',
+        function(error, result){
+            if (error) throw error
+            let products = {}
+            for(i=0;i<result.length;i++){
+                products[result[i]['id']] = result[i];
+            }
+        
+            res.render('main', {
+                products: JSON.parse(JSON.stringify(products))
+             });
+
+        }
+    );
+
+    
     
 })
 
-app.get('/cat', function(req, res){
-    res.end('cat')
-})
-// end 
+app.get('/category', function(req, res){
+    let catID = req.query.id;
+    let category = new Promise(function(resolve, reject){
+        connection.query(
+            'SELECT * FROM category WHERE id='+catID,
+            function(error,result){
+                if(error) reject(error);
+                resolve(result);
+            }
+        );
+    });
+
+    let products = new Promise(function(resolve, reject){
+        connection.query(
+            'SELECT * FROM products WHERE category='+catID,
+            function(error, result){
+                if( error ) reject(error);
+                resolve(result);
+            }
+        );
+    });
+
+    Promise.all([category,products]).then(function(valve){
+        res.render('category', {
+            category:  JSON.parse(JSON.stringify(valve[0])),
+            products:  JSON.parse(JSON.stringify(valve[1]))
+        })
+    });
+    
+});
+
+app.get('/product', function(req,res){
+    let id = req.query.id
+    connection.query('SELECT * FROM products WHERE id='+id , function(error, result, fields){
+        if(error) throw error;
+        res.render('product', {
+            product: JSON.parse(JSON.stringify(result))
+        })
+    })
+});
+
